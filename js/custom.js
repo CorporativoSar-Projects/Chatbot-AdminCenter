47bf62c962db6251b85eb4c6082ec4e07daa7e3d
karladur <karla.gudetama@gmail.com>
2024-06-20 15:54:15 -0600

    temas de conversacion

--- js/custom.js
+++ js/custom.js
@@ -49,3 +49,55 @@ txtNombreChat.addEventListener('keyup', () => {
     divCopiaNombre.innerHTML = txtNombreChat.value;
 });
 
+
+
+function agregarTema() {
+    const listaTemas = document.getElementById('listaTemas');
+    const temasActuales = listaTemas.getElementsByTagName('li');
+
+    
+    if (temasActuales.length >= 5) {
+        // alert('Solo puedes agregar hasta 5 temas de conversación.');
+        return;
+    }
+
+    // Crear el nuevo elemento de tema
+    const nuevoTema = document.createElement('li');
+    nuevoTema.className = 'tema-item';
+
+    // Crear el input para el nuevo tema
+    const nuevoInput = document.createElement('input');
+    nuevoInput.type = 'text';
+    nuevoInput.name = 'inp-conversa';
+    nuevoInput.placeholder = 'Nuevo tema de conversación';
+    nuevoInput.className = 'inp-conversa';
+    nuevoInput.required = true;
+
+ 
+    const botonBorrar = document.createElement('button');
+    botonBorrar.className = 'btn-borrar';
+    botonBorrar.onclick = function() {
+        eliminarTema(botonBorrar);
+    };
+
+ 
+    const imagenBorrar = document.createElement('img');
+    imagenBorrar.src = 'img/trash.png';
+    imagenBorrar.width = 20;
+    imagenBorrar.alt = 'Delete Topic';
+
+    botonBorrar.appendChild(imagenBorrar);
+
+  
+    nuevoTema.appendChild(nuevoInput);
+    nuevoTema.appendChild(botonBorrar);
+
+ 
+    listaTemas.appendChild(nuevoTema);
+}
+
+
+function eliminarTema(elemento) {
+    const temaItem = elemento.parentNode;
+    temaItem.remove();
+}
