63f35097efce2c10f69a9f021d2c29fdb063c75e
JessiMo1 <jessimo22311@gmail.com>
2025-12-12 18:25:58 -0600

    Modificación de menú incluyendo la guia

--- pantallaDespedida.php
+++ pantallaDespedida.php
@@ -185,6 +185,11 @@ $id_chatbot = $_SESSION['id_chatbot'] ?? null;
     <script src="js/guardadoGeneral.js"></script>
     <script src="js/menuLateral.js" type="module"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/driver.js@1.3.1/dist/driver.css" />
+    <link rel="stylesheet" href="css/tour_ixah.css">
+    <script src="https://cdn.jsdelivr.net/npm/driver.js@1.3.1/dist/driver.js.iife.js"></script>
+    <script src="js/tour_ixah.js"></script>
+
 
 </body>
 
