08c094b01425e63c093cd18f962e89776bcb35e3
JessiMo1 <jessimo22311@gmail.com>
2025-12-26 15:28:47 -0600

    Cambios para de recarga de estilos

--- estilo.php
+++ estilo.php
@@ -32,6 +32,7 @@ if ($row['total'] >= 1 && isset($_GET['nuevo'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js/cargaEstilos.js"></script>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/sty.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
